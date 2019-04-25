017770938c292d0c48df282f10ff997e55a28a2f
Jaqueline Kirino <jaqueline.kirino@soaexpert.com.br>
2019-04-25 15:21:41 -0300

    refac two sums using hash table

--- 01. Two Sum/README.md
+++ 01. Two Sum/README.md
@@ -2,3 +2,14 @@
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Solution:
+
+## 1. Using a for loop inside another for loop - O(n^2) 
+
+Then compare inside the second loop if the target is equal to the sum of the nums
+
+## 2. Using a for loop and a hash table
+
+Each number will have a complement number (number + complement = target ) and will save inside a hash table with its index.
+Each loop will search for the complement in the hashtable, and if the hashtable has the complement property will return the indexes.
--- 01. Two Sum/index.js
+++ 01. Two Sum/index.js
@@ -3,13 +3,27 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-  for (var i = 0; i < nums.length; i++) {
-      for (var j = i + 1; j < nums.length; j++) {
-          if (target === nums[i] + nums[j]) {
-              return [i, j];
-          }
+
+ // 1.
+const twoSum =  (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target === nums[i] + nums[j]) {
+        return [i, j];
       }
+    }
   }
-  
+};
+
+// 2.
+const twoSum =  (nums, target) => {
+  let mapNums = {}
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i]
+    if (mapNums.hasOwnProperty(complement)) {
+      return [mapNums[complement], i];
+    }
+
+    mapNums[nums[i]] = i
+  }
 };
