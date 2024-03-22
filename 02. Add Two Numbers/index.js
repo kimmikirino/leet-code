9d7c48fdb4c9f98bac2de1db0dc04195c4c7a3e3
Jaqueline Kimmi Kirino <bfatskih@gmail.com>
2024-03-22 17:48:19 -0300

    refac: add a new version of the add TwoNumbers using ListNode

--- 02. Add Two Numbers/index.js
+++ 02. Add Two Numbers/index.js
@@ -28,3 +28,24 @@ var addTwoNumbers = function(l1, l2) {
   return sum.toString().split('').reduce((next, val) => ({ val, next }), null)
 };
 
+var addTwoNumbers2 = function (l1, l2) {
+    let dummy = new ListNode(null)
+    let result = dummy
+    let carry = 0
+    while (l1 || l2 || carry) {
+        
+        const val1 = l1?.val || 0;
+        const val2 = l2?.val || 0;
+
+        const sum = val1 + val2 + carry;
+
+        carry = Math.floor(sum / 10)
+        result.next = new ListNode(sum % 10)
+        result = result.next
+
+        l1 = l1?.next;
+        l2 = l2?.next;
+    }
+
+    return dummy.next
+};
